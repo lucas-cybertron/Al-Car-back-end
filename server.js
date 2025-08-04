f6624fe3ffb1dc70400ecd98f2b07ff2affd72bb
LucasAPaiva <paivalucas8456@gmail.com>
2025-08-04 14:41:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const motoRoutes = require('./routes/motoRouter');
 // Middlewares
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://al-car-front-end.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
